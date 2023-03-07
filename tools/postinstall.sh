25a3a2aa5421e64c6e0970bf7d61783f0ea2a42e
Valen Ramos <valen.ramosmontero@gmail.com>
2023-03-07 10:34:38 -0300

    Terminal Configuration

--- tools/postinstall.sh
+++ tools/postinstall.sh
@@ -52,16 +52,14 @@ function install_dependencies() {
     trap 'error_handler "$BASH_COMMAND" "$?"' ERR
     echo -e "${YELLOW}${BOLD}[*] ${RESET}Installing Dependencies..."
     sudo pacman -Syu --noconfirm > /dev/null 2>&1
-    sudo pacman -Sq --noconfirm xorg-server xorg-xsetroot xorg-xrandr xorg-xinit xf86-video-qxl python openssh bspwm sxhkd rofi nitrogen picom kitty chromium ttf-hack-nerd firefox zsh git bat lsd neovim ranger > /dev/null 2>&1
+    sudo pacman -Sq --noconfirm xorg-server xorg-xsetroot xorg-xrandr xorg-xinit flameshot pavucontrol pulseaudio htop nautilus discord obs-studio libreoffice p7zip zip unzip pacman-contrib neofetch xf86-video-qxl python openssh bspwm sxhkd rofi nitrogen picom kitty chromium ttf-hack-nerd firefox zsh git bat lsd neovim ranger npm > /dev/null 2>&1
     sleep 1
     mkdir $HOME/.repos
     mkdir $HOME/.repos/yay
-    git clone -q https://aur.archlinux.org/yay-git.git ~/.repos/yay > /dev/null 2>&1
+    git clone -q https://aur.archlinux.org/yay-git.git $HOME/.repos/yay > /dev/null 2>&1
     cd $HOME/.repos/yay
     makepkg --noconfirm -si > /dev/null 2>&1
     cd
-    git clone -q --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10 > /dev/null 2>&1
-    echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc #will be removed later on
     yay --noconfirm -S polybar > /dev/null 2>&1
     tput cuu1
     tput ed
@@ -79,6 +77,9 @@ function clone_configs() {
         cp -r "$folder" "$Dotfiles_Dir"
     done
     cp $HOME/.repos/dotfiles/dotfiles/xinitrc $HOME/.xinitrc
+    cp $HOME/.repos/dotfiles/dotfiles/.zshrc $HOME/.zshrc
+    cp $HOME/.repos/dotfiles/dotfiles/.p10k.zsh $HOME/.p10k.zsh
+    cp $HOME/.repos/dotfiles/dotfiles/.root-p10k.zsh $HOME/.root-p10k.zsh
     mkdir $HOME/Documents
     cp -r $HOME/.repos/dotfiles/Wallpapers $HOME/Documents
     mkdir $HOME/Downloads
@@ -88,6 +89,14 @@ function clone_configs() {
     chmod +x $HOME/.config/polybar/scripts/network.sh
     chmod +x $HOME/.config/polybar/scripts/performance_counters.sh
     sed -i 's|# setwp|nitrogen --set-zoom-fill --save $HOME/Documents/Wallpapers/lake.jpg|g' $HOME/.xinitrc
+    git clone -q --depth=1 https://github.com/powerline/fonts.git $HOME/.repos/f > /dev/null 2>&1
+    git clone -q --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/themes/powerlevel10k > /dev/null 2>&1
+    sudo mkdir /usr/share/zsh/plugins
+    sudo git clone -q https://github.com/zsh-users/zsh-syntax-highlighting /usr/share/zsh/plugins/zsh-syntax-highlighting > /dev/null 2>&1
+    sudo git clone -q https://github.com/zsh-users/zsh-autosuggestions /usr/share/zsh/plugins/zsh-autosuggestions > /dev/null 2>&1
+    git clone -q https://github.com/ohmyzsh/ohmyzsh.git $HOME/.repos/ohmyzsh > /dev/null 2>&1
+    sudo mkdir /usr/share/zsh/plugins/zsh-sudo
+    sudo cp $HOME/.repos/ohmyzsh/plugins/sudo/sudo.plugin.zsh /usr/share/zsh/plugins/zsh-sudo/sudo.plugin.zsh
     tput cuu1
     tput ed
     echo -e "${GREEN}${BOLD}[*] ${RESET}Dotfiles"
@@ -100,8 +109,6 @@ function setup_aliases() {
     local ls_alias='alias ls="lsd -la"'
     echo "$cat_alias" >> $HOME/.bashrc
     echo "$ls_alias" >> $HOME/.bashrc
-    echo "$cat_alias" >> $HOME/.zshrc
-    echo "$ls_alias" >> $HOME/.zshrc
     alias cat='bat'
     alias ls='lsd -la'
     tput cuu1
@@ -118,7 +125,7 @@ function set_res() {
 sudo_request
 clear
 show_prompt
-install_dependencies
+#install_dependencies
 clone_configs
 setup_aliases
 cleanup
